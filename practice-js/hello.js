e714f1b55f23c2a1c3de2f6e650a05f3ba3f2da0
G184792021 <218479@st.takushoku-u.ac.jp>
2022-05-12 12:42:39 +0900

    :

--- practice-js/hello.js
+++ practice-js/hello.js
@@ -1,1 +1,13 @@
 console.log('こんにちは');
+let x;
+undefined
+x=3;
+3
+X=x+1;
+4
+x;
+3
+x=x+1
+4
+cpnsole.log(x);
+4
---
+++ project.js
@@ -0,0 +1,7 @@
+let japan = {
+    uname: "日本",
+    weather.description: "",
+    main.temp_min:"",
+    main.temp_max:"",
+    main.humidity:"",
+}
